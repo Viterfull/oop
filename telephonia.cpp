13213ea9e7299360db1becd058a715fd2f6cc03a
Viterfull <100037636+Viterfull@users.noreply.github.com>
2023-03-30 17:03:54 +0300

    Update telephonia.cpp

--- telephonia.cpp
+++ telephonia.cpp
@@ -1,42 +1,55 @@
-#include "telephonia.h"
-#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
 
-country *global_telephonia::create_country(std::string name){
-    country a(name);
-    this->countryes_[name] = a;
-    std::cout << "создана страна " << name << "\n";
-    return &(this->countryes_[name]);
-}
+class number{
+private:
+    std::string name_;
+    std::vector <std::string> messeges_;
+    
+public:
+    number (std::string name)
+        : name_(name){};
+    number ()
+        : name_("empty"){};
+    std::string get_number();
+};
 
-operat *country::create_operat(std::string name){
-    operat a(name);
-    this->operators_[name] = a;
-    std::cout << "создан оператор " << name << "\n";
-    return &(this->operators_[a]);
-}
+class operat{
+private:
+    std::string name_;
+    std::map <std::string, number> numbers_;
+public:
+    operat (std::string name)
+        : name_(name){};
+    operat ()
+        : name_("empty"){};
+    void create_abonent(number name);
+};
 
-void operat::create_abonent(number a){
-    std::cout << "создан номер " << a.get_number() << "\n";
-    this->numbers_[a.get_number()] = a;
-}
+class country {
+private:
+    std::string name_;
+    std::map <std::string, operat> operators_; 
+public:
+    country (std::string name)
+      : name_(name){};
+    country ()
+      : name_("empty"){};
+    operat *create_operat(std::string name);
+    void send(std::string adres, std::string messege);
+};
 
-void global_telephonia::send(std::string adres, std::string messege){
-    std::string s = adres;
-    s.erase(0, 1);
 
-    std::reverse(s.begin(), s.end());
-    std::string countr = s.substr(10);
-    std::reverse(countr.begin(), countr.end());
-    countr = "+" + countr;
+class global_telephonia {
+private:
+    std::map <std::string, country> countryes_;
 
-    s.erase(10, s.length());
-    std::string opp = s.substr(7);
-    std::reverse(opp.begin(), opp.end());
-    std::cout << "страна/оператор адресата " << countr << "\\" << opp << "   " << s <<"\n";
-    //this->countryes_[countr].send(opp, adres, messege);
-}
-// 12 34 567 988
 
-std::string number::get_number(){
-    return this->name_;
-}
+public:
+    country *create_country(std::string name);
+    void send(std::string adres, std::string messege);
+};
+
+
+
